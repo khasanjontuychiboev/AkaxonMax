8867b5604ed4093ecad59cf3434d5198100a4b81
Xasanboy <ucbeyibenim@gmail.com>
2023-09-09 15:26:30 +0500

    First Commit

---
+++ src/main/java/com/akaxon/akaxonmax/AkaxonMaxApplication.java
@@ -0,0 +1,13 @@
+package com.akaxon.akaxonmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkaxonMaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkaxonMaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akaxon/akaxonmax/AkaxonMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akaxon.akaxonmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkaxonMaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
